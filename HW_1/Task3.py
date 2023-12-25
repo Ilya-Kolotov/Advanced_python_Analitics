4b9a847c29ec709a7ed6007cc906a433e1ec9601
Ilya.Kolotov <kolotov.iljya@gmail.com>
2023-12-25 16:06:37 +0500

    ADD Task 3

--- HW_1/Task3.py
+++ HW_1/Task3.py
@@ -6,20 +6,23 @@
 from random import
 randint num = randint(LOWER_LIMIT, UPPER_LIMIT)
 """
+LOWER_LIMIT = 0
+UPPER_LIMIT = 1000
+ATTEMPTS = 10
 from random import randint
-num = randint(0, 1000)
-print('Угадайте число от 0 до 1000 за 10 попыток')
-attempt = 10
+num = randint(LOWER_LIMIT, UPPER_LIMIT)
+print(f'Угадайте число от {LOWER_LIMIT} до {UPPER_LIMIT} за {ATTEMPTS} попыток')
+attempt = ATTEMPTS
 while attempt > 0:
-    x = int(input(f'У вас осталось {attempt} попыток. Введите число: '))
+    x = int(input(f'У вас осталось {attempt} попыток(-ки).\nВведите число: '))
     if x == num:
         print('Поздравляем. Вы угадали число')
         attempt = -1
     else:
         if x > num:
-            print('Вы вели число больше загаданного')
+            print('Вы ввели число больше загаданного')
         else:
-            print('Вы вели число меньше загаданного')
+            print('Вы ввели число меньше загаданного')
     attempt -= 1
 if attempt == 0:
     print('Увы. Попытки кончились')
